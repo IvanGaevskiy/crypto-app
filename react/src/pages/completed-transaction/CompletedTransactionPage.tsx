a7e0a4c20e1221cc605a31ebed2e97d5e98be378
night cat <gl_serius_sam@mail.ru>
2025-04-07 21:37:26 +0400

    added routing

---
+++ react/src/pages/completed-transaction/CompletedTransactionPage.tsx
@@ -0,0 +1,8 @@
+export const CompletedTransactionPage = () => {
+  return (
+    <div>
+      <h1>Completed Transaction Page</h1>
+      <p>This is the completed transaction page.</p>
+    </div>
+  );
+}
---
+++ react/src/pages/exchange/ExchangePage.tsx
@@ -0,0 +1,8 @@
+export const ExchangePage = () => {
+  return (
+    <div>
+      <h1>Exchange Page</h1>
+      <p>This is the exchange page.</p>
+    </div>
+  );
+}
