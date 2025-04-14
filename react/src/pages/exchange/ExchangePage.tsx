e7a81c95c758b06058fa801c8f6f154593ee95b7
night cat <gl_serius_sam@mail.ru>
2025-04-15 02:19:34 +0400

    deleted old method

--- react/src/pages/exchange/ExchangePage.tsx
+++ react/src/pages/exchange/ExchangePage.tsx
@@ -54,30 +54,6 @@ export const ExchangePage = () => {
   const { curr: currFrom, currColor: currColorFrom, currBorder: currBorderFrom } = currencyFrom
   const { curr: currTo, currColor: currColorTo, currBorder: currBorderTo } = currencyTo
 
-  // const startExchangeSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault()
-  //   console.log('Начинаем валидацию...')
-
-  //   console.log('amountFrom:', isValidAmountFrom(amountFrom, minFrom, maxFrom))
-  //   console.log('amountTo:', isValidAmountTo(amountTo, minTo, maxTo))
-  //   console.log('purposePay:', isValidPurposePay(purposePay))
-  //   console.log('email:', isValidEmail(email))
-  //   console.log('KYC & AML:', isValidKYCAndAML([isKYC, isAML]))
-
-  //   if (
-  //     !isValidAmountFrom(amountFrom, minFrom, maxFrom) &&
-  //     !isValidAmountTo(amountTo, minTo, maxTo) &&
-  //     !isValidPurposePay(purposePay) &&
-  //     !isValidEmail(email) &&
-  //     !isValidKYCAndAML([isKYC, isAML])
-  //   ) {
-  //     console.log('Все валидации пройдены успешно!')
-  //     push('/completed_transaction')
-  //   } else {
-  //     console.log('Некоторые валидации не пройдены :(')
-  //   }
-  // }
-
   const startExchangeSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     console.log('Начинаем валидацию...')
