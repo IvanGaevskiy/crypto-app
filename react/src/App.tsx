3a6851759c3a556f4d5e7bb97274094b9a60a270
night cat <gl_serius_sam@mail.ru>
2025-04-14 18:00:24 +0400

    main.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,3 @@
-import { createRoot } from 'react-dom/client'
-
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 import './App.css'
@@ -8,30 +6,30 @@ import { NotFoundPage } from './components/NotFoundPage'
 import { CompletedTransactionPage } from './pages/completed-transaction/CompletedTransactionPage'
 import { ExchangePage } from './pages/exchange/ExchangePage'
 
-createRoot(document.getElementById('root')!).render(
-  // <div className="flex min-h-[100dvh] w-full items-center justify-center">
-  //   <img className="absolute h-full w-full" src="./src/assets/space.svg"></img>
-  <div className="flex min-h-[100dvh] w-full items-center justify-center">
-    <div className="absolute top-0 z-0 h-[120%] w-full overflow-hidden ">
-      <img
-        className="absolute block h-auto w-full bottom-1/12 transform [-translate-x:50%] [translate-y:16%]"
-        src="./src/assets/space.svg"
-      ></img>
-      <img
-        className="absolute block h-auto w-full bottom-0 transform [-translate-x:50%] [translate-y:16%]"
-        src="./src/assets/planets.svg"
-      ></img>
+export const App = () => {
+  return (
+    <div className="mt-12 flex min-h-[100dvh] w-full items-baseline justify-center">
+      <div className="absolute top-0 z-0 h-[120%] w-full overflow-hidden">
+        <img
+          className="absolute bottom-1/12 block h-auto w-full transform [-translate-x:50%] [translate-y:16%]"
+          src="./src/assets/space.svg"
+        ></img>
+        <img
+          className="absolute bottom-0 block h-auto w-full transform [-translate-x:50%] [translate-y:16%]"
+          src="./src/assets/planets.svg"
+        ></img>
+      </div>
+      <div className="relative z-10">
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<NotFoundPage />} />
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<ExchangePage />} />
+              <Route path="/completed_transaction" element={<CompletedTransactionPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
-    <div className="relative z-10">
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<NotFoundPage />} />
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<ExchangePage />} />
-            <Route path="/completed_transaction" element={<CompletedTransactionPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  </div>
-)
+  )
+}
