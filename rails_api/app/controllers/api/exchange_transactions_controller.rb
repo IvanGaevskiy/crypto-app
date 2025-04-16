d37b6f12c43ada0df73dfee01d395cb3e1892a4f
night cat <gl_serius_sam@mail.ru>
2025-04-16 15:01:49 +0400

    linters

--- rails_api/app/controllers/api/exchange_transactions_controller.rb
+++ rails_api/app/controllers/api/exchange_transactions_controller.rb
@@ -1,26 +1,26 @@
 class Api::ExchangeTransactionsController < ApplicationController
-  def show
-    @exchangeTransaction = ExchangeTransaction.new(transaction_params)
+  def create
+    exchange_transaction = ExchangeTransaction.new(transaction_params)
 
     exchange_rates_params.each do |rate_params|
-      @exchangeTransaction.exchange_rate.build(rate_params)
+      exchange_transaction.exchange_rate.build(rate_params)
     end
 
     agreetments_params.each do |agreetment_param|
-      @exchangeTransaction.agreetment.build(agreetment_param)
+      exchange_transaction.agreetment.build(agreetment_param)
     end
 
-    if @exchange_transaction.save
-      render json: { status: "success", transaction: @exchange_transaction.as_json }
+    if exchange_transaction.save
+      render json: { status: "success", transaction: exchange_transaction.as_json }
     else
-      render json: { status: "error", errors: @exchange_transaction.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: "error", errors: exchange_transaction.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def transaction_params
-    params.permit(:recipient_address, :email, currency_amounts: [ :currency, :amount ])
+    params.permit(:recipient_address, :email, currency_amounts: [:currency, :amount])
   end
 
   def exchange_rates_params
